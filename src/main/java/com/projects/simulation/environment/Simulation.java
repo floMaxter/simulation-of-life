f23cf51f2fe257be7df2c3198bb2362ab02492a4
Max Vdovin <maxpostupaet@gmail.com>
2024-11-21 09:52:31 +0300

    fix: fix the order of execution of the program cycle in the area of processing commands from the user. Remove the looping when processing the user's choice.

--- src/main/java/com/projects/simulation/environment/Simulation.java
+++ src/main/java/com/projects/simulation/environment/Simulation.java
@@ -1,18 +1,15 @@
 package com.projects.simulation.environment;
 
-import com.projects.simulation.action.init.PredatorSpawnAction;
-import com.projects.simulation.action.init.RockSpawnAction;
-import com.projects.simulation.action.init.TreeSpawnAction;
-import com.projects.simulation.entity.inanimate.Rock;
-import com.projects.simulation.utils.GameUtils;
 import com.projects.simulation.action.Action;
 import com.projects.simulation.action.init.GrassSpawnAction;
 import com.projects.simulation.action.init.HerbivoreSpawnAction;
+import com.projects.simulation.action.init.PredatorSpawnAction;
+import com.projects.simulation.action.init.RockSpawnAction;
+import com.projects.simulation.action.init.TreeSpawnAction;
 import com.projects.simulation.action.turn.MoveAction;
-import com.projects.simulation.entity.animate.Herbivore;
-import com.projects.simulation.entity.inanimate.Grass;
 import com.projects.simulation.io.ConsoleManager;
 import com.projects.simulation.render.WorldMapRender;
+import com.projects.simulation.utils.GameUtils;
 import com.projects.simulation.utils.MenuOptions;
 
 import java.util.ArrayList;
@@ -21,21 +18,23 @@ import java.util.Optional;
 
 public class Simulation {
 
-    private WorldMap worldMap;
     private final Integer moveCount;
+    private boolean isSimulationRunning;
+    private WorldMap worldMap;
     private final ConsoleManager consoleManager;
     private final WorldMapRender worldMapRender;
-    private List<Action> initActions;
+    private final List<Action> initActions;
 
     public Simulation() {
         this.moveCount = 0;
+        isSimulationRunning = false;
         this.consoleManager = new ConsoleManager();
         this.worldMapRender = new WorldMapRender();
+        this.initActions = new ArrayList<>();
         generateInitActions();
     }
 
     private void generateInitActions() {
-        this.initActions = new ArrayList<>();
         initActions.add(new GrassSpawnAction());
         initActions.add(new RockSpawnAction());
         initActions.add(new TreeSpawnAction());
@@ -46,62 +45,50 @@ public class Simulation {
     public void startGame() {
         consoleManager.printWelcomeWords();
         createNewMap();
-        executeGameLoop();
-    }
 
-    private void createNewMap() {
-        this.worldMap = new WorldMap(GameUtils.WORLD_MAP_HEIGHT, GameUtils.WORLD_MAP_WIDTH);
-        for (Action action : initActions) {
-//            action.perform(worldMap);
+        boolean isRunning = true;
+        while (isRunning) {
+            updateUI();
 
-            worldMap.setEntity(new Cell(9, 1), new Herbivore());
-
-            worldMap.setEntity(new Cell(1, 10), new Grass());
-
-            worldMap.setEntity(new Cell(1, 2), new Rock());
-            worldMap.setEntity(new Cell(2, 2), new Rock());
-            worldMap.setEntity(new Cell(3, 2), new Rock());
-            worldMap.setEntity(new Cell(4, 2), new Rock());
-            worldMap.setEntity(new Cell(5, 2), new Rock());
-            worldMap.setEntity(new Cell(6, 2), new Rock());
-            worldMap.setEntity(new Cell(7, 2), new Rock());
-            worldMap.setEntity(new Cell(8, 2), new Rock());
-            worldMap.setEntity(new Cell(9, 2), new Rock());
-        }
-    }
-
-    private void executeGameLoop() {
-        worldMapRender.renderMap(worldMap);
-        consoleManager.printGameFeatures();
-        processUserInput();
-    }
-
-    private void processUserInput() {
-        while (true) {
             int userChoice = consoleManager.readUserInput();
             Optional<MenuOptions> selectedOption = MenuOptions.fromNumber(userChoice);
-
             if (selectedOption.isPresent()) {
-                switch (selectedOption.get()) {
-                    case OPTION_ONE_MOVE -> nextTurn();
-                    case OPTION_ENDLESS_CYCLE -> startSimulation();
-                    case OPTION_PAUSE_SIMULATION -> pauseSimulation();
-                    case OPTION_NEW_MAP -> createNewMap();
-                    case OPTION_EXIT -> {
-                        endGame();
-                        return;
-                    }
-                }
+                isRunning = processUserInput(selectedOption.get());
             } else {
                 System.out.println("Invalid option. Please try again");
             }
         }
     }
 
+    private void createNewMap() {
+        this.worldMap = new WorldMap(GameUtils.WORLD_MAP_HEIGHT, GameUtils.WORLD_MAP_WIDTH);
+        for (Action action : initActions) {
+            action.perform(worldMap);
+        }
+    }
+
+    private void updateUI() {
+        worldMapRender.renderMap(worldMap);
+        consoleManager.printGameFeatures();
+    }
+
+    private boolean processUserInput(MenuOptions options) {
+        switch (options) {
+            case OPTION_ONE_MOVE -> nextTurn();
+            case OPTION_ENDLESS_SIMULATION -> startSimulation();
+            case OPTION_PAUSE_SIMULATION -> pauseSimulation();
+            case OPTION_NEW_MAP -> createNewMap();
+            case OPTION_EXIT -> {
+                endGame();
+                return false;
+            }
+        }
+        return true;
+    }
+
     private void nextTurn() {
         MoveAction moveAction = new MoveAction();
         moveAction.perform(worldMap);
-        executeGameLoop();
     }
 
     private void startSimulation() {
@@ -109,7 +96,8 @@ public class Simulation {
     }
 
     private void pauseSimulation() {
-
+        System.out.println("Simulation paused. Choose an option to continue.");
+        isSimulationRunning = false;
     }
 
     private void endGame() {
